92714f472dff74c15c766bec19ac7fcf695beffa
EmreZiyaEfe <emreziyae@gmail.com>
2023-01-20 22:54:31 +0300

    (20 Ocak Ders Sonu) JS değişkenler ve veri türleri

--- JavaScript/18 Ocak Js D1/app.js
+++ JavaScript/18 Ocak Js D1/app.js
@@ -1,4 +1,7 @@
 //!console.log()
 //*console log ekrana veri basmak veya mesaj vermek için kullanılır
+
+
 console.log("Hello World!")
 console.log("Hi I'm Emre")
+
---
+++ JavaScript/20 Ocak D2/app.js
@@ -0,0 +1,83 @@
+//ekrana mesaj yazdırma
+
+/*console.log("selam")
+console.log("ben emre")
+
+//! js yorum satırları
+
+//yorum satırı var
+
+/* 
+
+birden fazla 
+yorum satırı
+
+*/
+
+//!değişkenler
+
+/*let name1 = "emre";
+let surname = "efe";
+let alisVerisMiktari = 100;
+
+console.log(name1);
+console.log(surname);
+
+console.log(`Alışverişiniz Tamamlandı. Sayın ${name1} ${surname}! Alışveriş tutarı: ${alisVerisMiktari}`);*/
+
+//?örnek ad soyad ve yas yazınız
+
+/*let ad = "emre";
+let soyad = "efe";
+let yas = 26;
+
+console.log(ad);
+console.log(soyad)
+console.log(yas)
+console.log(`${ad} ${soyad} ${yas}`);
+console.log(ad, soyad, yas);*/
+
+//*SyntaxError: kullanım hatası
+
+//!değişken oluşturma kuralları
+
+//? sayısal ifade ile başlamaz
+//? değişkenler arasında boşluk olmaz
+//? değişkenler arasına özel karakterler kullanılmaz
+
+
+//! veri türleri
+//? sayısal veri türü (number)
+
+// let yas = 26 //*sayısal
+// let yil = 2023 //*sayısal
+
+// console.log(yas, yil)
+
+//? metinsel veri türü (string)
+
+// let isim = "emre" //* metinsel
+
+// console.log(isim)
+
+// let yas = "26";
+// let yas2 = "27"
+// console.log(yas+yas2)
+
+// let sayi1 = 26;
+// let sayi2 = 55;
+// console.log(sayi1+sayi2)
+
+// let sayi1 = "26";
+// let sayi2 = 33;
+// console.log(sayi1+sayi2)
+
+// let isim = "emre"
+// let soyisim = "efe"
+// let yas = 26;
+// console.log(isim, soyisim, yas)
+
+//!typeof ()
+//? veri tipleri döndürür
+let isim = "emre"
+console.log(typeof(isim))

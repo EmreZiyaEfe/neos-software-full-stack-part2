93ada8a3b96f5b013b5f7c71a42ffc9d600f1b9e
EmreZiyaEfe <emreziyae@gmail.com>
2023-02-13 22:35:32 +0300

    (13 Şubat Ders Sonu) JS döngüler (for)

---
+++ JavaScript/13 Şubat D8/ders8.js
@@ -0,0 +1,42 @@
+//?for
+
+// for(let i = 0; i < 10 ; i++) {
+//     console.log("emre");
+// }
+
+// for (let i = 0; i < 10 ;i++) {
+//     console.log(i);
+// }
+
+//kullanıcıya 3 kere sınav sor
+
+// for (let i = 0; i < 3 ; i++) {
+//     girilenSinav = Number(prompt("sınavı girin"));
+//     console.log(girilenSinav);
+// }
+
+// for (let i = 0; i<=10; i++) {
+//     console.log(i);
+//     if (i==4){
+//         break;
+//     }
+// }
+
+// for (let i = 0; i<=5; i++) {
+//     if (i == 0 && i == 4) {
+//         console.log(i)
+//     }
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//     alert(i);
+// }
+
+//? continue (döngüyü başa alır)
+
+for (let i = 0; i <=10; i++) {
+    if (i == 4) {
+        continue
+    }
+    console.log(i);
+}

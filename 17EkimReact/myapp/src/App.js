aa11a0fffeb70ac3ee2d13ec3b70371e1242f04f
EmreZiyaEfe <emreziyae@gmail.com>
2023-03-03 20:47:26 +0300

    (1 Mart Ders Sonu) React intro

--- 17EkimReact/myapp/src/App.js
+++ 17EkimReact/myapp/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Urunbox from './Components/Urunbox';
 
 function App() {
+
+  let x = 20;
+  let y = 30; 
+  let z = 50;
+  const sonuc = (x + y + z) / 3;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>selam bu ilk projemiz</h1>
+      <h2 className="test">Emre</h2> */}
+
+      {/* <h3>İki Sayının Toplamı : {x+y}</h3> */}
+
+      {/* <h3>Ortalama : {sonuc}</h3> */}
+
+      <Urunbox/>
+
     </div>
   );
 }
---
+++ 17EkimReact/myapp/src/Components/Urunbox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+        <h3>Çorap</h3>
+        <h6>14 Günün En Düşük Fiyatı</h6>
+        <h2>140 ₺</h2>
+    </div>
+  )
+}
